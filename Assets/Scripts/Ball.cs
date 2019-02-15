7a5617c027c5c81c2af7d9ff96fc0f957065b0de
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-02-16 03:31:38 +0600

    Launched the ball from paddle

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
     [SerializeField] Paddle paddle;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
 
     Vector2 paddleToBallVector;
 
+    bool hasStarted;
+
     void Start() {
         paddleToBallVector = transform.position - paddle.transform.position;
+
+        hasStarted = false;
     }
 
     void Update() {
-        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        if(!hasStarted) {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LockBallToPaddle() {
+        Vector2 paddlePos = new Vector2 (paddle.transform.position.x, paddle.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void LaunchOnMouseClick() {
+        if(Input.GetMouseButtonDown(0)) {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+
+            hasStarted = true;
+        }
     }
 }
