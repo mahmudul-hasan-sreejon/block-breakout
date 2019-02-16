0bd486629d9a36bb75086d8ec65515ef5df47450
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-02-17 04:25:19 +0600

    Block destroy functionality added

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+        
+        Debug.Log(other.gameObject.name);
+    }
+}
