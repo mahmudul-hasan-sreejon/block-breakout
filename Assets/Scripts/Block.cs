b9bc6c257a86f62f8fd271684f22421516480884
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-03-01 16:20:19 +0600

    Game win logic added

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,13 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
     [SerializeField] private AudioClip breakSound;
+    
+    // Cached reference
+    Level level;
+
+    
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
+
+        level.BlockDestroyed();
     }
 }
